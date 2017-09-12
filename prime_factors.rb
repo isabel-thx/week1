858b486ba55c47f30506e94a1467155d9798a316
isabel-thx <isabelhx.teh@gmail.com>
2017-09-12 12:19:25 +0800

    first commit

---
+++ prime_factors.rb
@@ -0,0 +1,16 @@
+  def prime_factors(n)
+	result = []
+  until n == 1
+  	for i in (2..n)
+    	if n % i == 0
+    		result.push i
+      	n = n / i
+      	break
+    	end
+    end
+  end
+  return result
+end
+p prime_factors(12056)
+
+
