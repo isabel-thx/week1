96652bf72ca07342861894666e889dfb620981d0
Isabel Teh <isabelhx.teh@gmail.com>
2017-09-12 18:18:17 +0800

    Update method_chaining.rb

--- method_chaining.rb
+++ method_chaining.rb
@@ -5,13 +5,11 @@ end
 
 
 def factors_of(x)
-  (2..x-1).select{|i| x % i == 0}		#figure out if number can be divided by anything except itself
-end																	#select number if it can only be divided by itself to == 0
+  (2..x-1).select{|i| x % i == 0}				#figure out if number can be divided by anything except itself
+end								#select number if it can only be divided by itself to == 0
 																		
 def squared_primes(array)
-  primes = array.select{|x| 
-    factors_of(x).count == 0				#i.e. select if array is empty = []
-  }
+  primes = array.select{|x| factors_of(x).count == 0}		#i.e. select if array is empty = []
   
   primes.map{|p| p*p}
 end
