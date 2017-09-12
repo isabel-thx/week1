043456483a91c6d34df77e183763fee4941c7635
Isabel Teh <isabelhx.teh@gmail.com>
2017-09-12 17:52:55 +0800

    Update pig_latin.rb

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,11 +1,25 @@
-def convert_word_to_pig_latin(word)
-	firstLetter = word[0]
-		if firstLetter.start_with?("a","e","i","o","u")
-  		word = word + "way"
-		else
-  		word.slice!(0)                          #this removes first letter from word
-  		word = word + firstLetter + "ay"
-		end
-	puts word
+def pig_latin(word)
+  firstLetter = word[0]
+    if firstLetter.start_with?("a","e","i","o","u")
+      word = word + "way"
+    else
+      word.slice!(0)                          #this removes first letter from word
+      word = word + firstLetter + "ay"
+    end
+    word
 end
-convert_word_to_pig_latin("apple")
+
+pig_latin("apple")
+
+
+
+def convert_sentence(sentence)
+
+  sentence_array = sentence.split(" ")
+  result = ""
+  sentence_array.each {|x| result += pig_latin(x) + " " }
+  return result
+
+end
+
+p convert_sentence("hello I am isabel")
