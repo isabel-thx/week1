c301c264684845f60eb6c87083466de6113c88b8
Isabel Teh <isabelhx.teh@gmail.com>
2017-09-12 17:59:56 +0800

    Update deaf_aunty.rb

--- deaf_aunty.rb
+++ deaf_aunty.rb
@@ -1,35 +1,35 @@
 def deaf_aunty
   puts "Talk to aunty"
-	reply = gets.chomp
-	while reply != "I love you aunty but I gtg"
-  	if reply == reply.upcase 
-    	puts "NO, WE CAN'T DO THAT!"
-    	reply = gets.chomp
-  	else
-    	puts "HUH? SPEAK UP SANDRA!"
-    	reply = gets.chomp
-  	end
-	end
+  reply = gets.chomp
+  while reply != "I love you aunty but I gtg"
+    if reply == reply.upcase 
+      puts "NO, WE CAN'T DO THAT!"
+      reply = gets.chomp
+    else
+      puts "HUH? SPEAK UP SANDRA!"
+      reply = gets.chomp
+    end
+  end
   puts "Aw alright Sandy, bye!"
 
 counter = 2
-	while counter > 0
-	reply = gets.chomp.to_s
-  	if reply == ""
-    	counter = counter-1
-    	if counter > 0
+  while counter > 0
+  reply = gets.chomp.to_s
+    if reply == ""
+      counter = counter-1
+      if counter > 0
+        puts ""
+      else
       	puts ""
-    	else
-      	puts ""
-    	end
-  	elsif reply == reply.upcase
-    	puts "NO, WE CAN'T DO THAT!"
-    	counter = 2                       #when "not" input, reset counter to start
-  	else
-    	puts "HUH? SPEAK UP SANDRA!"
-    	counter = 2                       #same here
-  	end
-	end
+      end
+    elsif reply == reply.upcase
+      puts "NO, WE CAN'T DO THAT!"
+      counter = 2                       #when "not" input, reset counter to start
+    else
+      puts "HUH? SPEAK UP SANDRA!"
+      counter = 2                       #same here
+    end
+  end
 end
 
 deaf_aunty
