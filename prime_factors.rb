764c58e1b05e890f62f34454f8ad80a664043548
Isabel Teh <isabelhx.teh@gmail.com>
2017-09-12 18:20:36 +0800

    Update prime_factors.rb

--- prime_factors.rb
+++ prime_factors.rb
@@ -1,16 +1,17 @@
-  def prime_factors(n)
-	result = []
+def prime_factors(n)
+  result = []
   until n == 1
-  	for i in (2..n)
-    	if n % i == 0
-    		result.push i
+    for i in (2..n)
+      if n % i == 0
+        result.push i
       	n = n / i
-      	break
-    	end
+        break
+      end
     end
   end
   return result
 end
+
 p prime_factors(12056)
 
 
