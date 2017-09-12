5c6318ae16d39e8807f51d05a5358a437b552f1a
Isabel Teh <isabelhx.teh@gmail.com>
2017-09-12 18:17:02 +0800

    Update manipulate_string.rb

--- manipulate_string.rb
+++ manipulate_string.rb
@@ -1,17 +1,17 @@
 def manipulate2(str,ltr)
   array_str = str.chars.to_a
   new_str = []
-  while array_str != []
-    if array_str[0] == ltr
-    array_str[0] = (array_str[0]).upcase
-    new_str.push array_str[0]
-    array_str.shift
-  else
-    array_str[0] = (array_str[0]).downcase
-    new_str.push array_str[0]
-    array_str.shift
+    while array_str != []
+      if array_str[0] == ltr
+        array_str[0] = (array_str[0]).upcase
+        new_str.push array_str[0]
+        array_str.shift
+      else
+        array_str[0] = (array_str[0]).downcase
+        new_str.push array_str[0]
+        array_str.shift
+      end
     end
-  end
   puts new_str.join
 end
 
