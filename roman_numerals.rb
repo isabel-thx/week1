dc980315b0c3e29e4c9665240fcefec0bf068580
Isabel Teh <isabelhx.teh@gmail.com>
2017-09-12 18:22:10 +0800

    Update roman_numerals.rb

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,7 +1,5 @@
 def to_roman(num)
-
   rome = ""
-
   symbol = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
   magnitude = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
   
@@ -10,10 +8,9 @@ def to_roman(num)
           x = num/magnitude[index]
           rome += sym * x
          num = num % magnitude[index]
-       end
-  end
-  
+     end
+  end 
 return rome
-  
 end
+
 p to_roman(194)
