d668bdf4e984d3814003851882dfede09f385451
isabel-thx <isabelhx.teh@gmail.com>
2017-09-12 12:33:40 +0800

    commit

---
+++ triangle.rb
@@ -0,0 +1,22 @@
+def triangle_up(num, char)
+	1.upto(num){|i|
+  	i.times do
+      print char
+    end
+  	print "\n"
+  }
+end
+triangle_up(5, 'x')
+
+puts
+
+def triangle_down(num, char)
+	num.downto(1){|i|
+  	i.times do
+      print char
+    end
+  	print "\n"
+  }
+end
+triangle_down(5, 'x')
+
